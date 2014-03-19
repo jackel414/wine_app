d06c66b6df39fbc6dba4262d86f8ecb2f31b5635
Zack <zackmays@gmail.com>
2014-03-19 17:21:19 -0400

    Add delete wine function and wine validation

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,6 +1,9 @@
 class Wine < ActiveRecord::Base
   belongs_to :user
   
+  validates_presence_of :name, :grapes, :region, :country, :on => :create
+  validates_presence_of :name, :grapes, :region, :country, :on => :update
+  
   scope :cellared, where(stored: true)
   scope :cataloged, where(stored: false)
 end
